5312f464903cc9bc74f7917e060b25393214654e
emartinello <enrico.martinello@h-en.me>
2024-07-25 15:36:52 +0200

    Implemented chunked deletion of records in the `directus_activity` table

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.1.0] - 2024-07-25
+### Added
+- Implemented chunked deletion of records in the `directus_activity` table, processing records in batches of 5000 to improve performance and reduce load on the database.
+
+## [1.0.0] - 2024-07-17
+### Added
+- Initial release of the project.
--- src/cleanup-activities.js
+++ src/cleanup-activities.js
@@ -7,7 +7,24 @@ const cleanupActivities = async (days) => {
         const date = new Date();
         date.setDate(date.getDate() - days);
 
-        await knex('directus_activity').where('timestamp', '<', date).del();
+        const chunkSize= 5000;
+        let deletedRecords;
+
+        do {
+            try {
+                deletedRecords = await knex('directus_activity')
+                    .where('timestamp', '<', date)
+                    .orderBy('timestamp')
+                    .limit(chunkSize)
+                    .del();
+
+                console.log(`Deleted ${deletedRecords} records`);
+            } catch (error) {
+                console.error('Error deleting records:', error);
+                break;
+            }
+        } while (deletedRecords > 0);
+
         console.log(`Table directus_activity with timestamps before ${date.toISOString()} have been cleaned up successfully.`);
     } catch (error) {
         console.error('Error cleaning up directus_activity table:', error);
